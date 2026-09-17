878ff36c9d3107f014984ee0c19b62094e4bc6d4
tevodich321-create <tevodich321@gmail.com>
2026-09-17 12:59:48 +0700

    Add PWA service worker

---
+++ site/sw.js
@@ -0,0 +1,1 @@
+const CACHE='nh-tts-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./app.js','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
