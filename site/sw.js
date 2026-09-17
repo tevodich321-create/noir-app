04a9f47d24ffd269ad40ed9c0360b3b73887a741
tevodich321-create <tevodich321@gmail.com>
2026-09-17 13:09:19 +0700

    Invalidate stale PWA cache for verified audio fix

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const CACHE='nh-tts-v5';
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./app.js?v=4','./manifest.webmanifest'])).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+const CACHE='nh-tts-v6';
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./app.js?v=5','./manifest.webmanifest'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{const url=new URL(event.request.url);if(url.origin!==location.origin)return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})))})
